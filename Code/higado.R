8612ff3758fef13ba8a1da1db6437b63e57da00f
ruhegui <ruhegui@gmail.com>
2024-10-29 16:41:43 +0100

    añadiendo hígado

--- Code/higado.R
+++ Code/higado.R
@@ -1,7 +1,8 @@
+#Verificar BiocManager
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 #Paquetes
-list.of.packages = c("tximeta", "tximport", "limma", "edgeR", "tidyverse", "org.Mm.eg.db", "statmod", "pheatmap", "ggplotify", "ggrepel", "SummarizedExperiment", "patchwork", "xlsx", "ragg", "OmnipathR", "clusterProfiler")
+list.of.packages = c("tximeta", "tximport", "limma", "edgeR", "tidyverse", "org.Mm.eg.db", "statmod", "pheatmap", "ggplotify", "ggrepel", "SummarizedExperiment", "patchwork", "xlsx", "ragg", "OmnipathR")
 #Instalación por CRAN o Bioconductor
 new.packages = list.of.packages[!(list.of.packages %in% installed.packages())]
 if(length(new.packages)> 0) {
@@ -14,28 +15,28 @@ if(length(new.packages)> 0) {
 invisible(lapply(list.of.packages, FUN=library, character.only=TRUE))
 rm(list.of.packages, new.packages)
 setwd("/home/guille/RNA/RNA_2024/EXPA/experimento_A")
-files = data.frame(read.table(file="Metadata/metadata.txt", header = TRUE, stringsAsFactors = F))[c(18:35),]
+files = data.frame(read.table(file="Metadata/metadata.txt", header = TRUE, stringsAsFactors = F))[c(36:53),]
 suppressPackageStartupMessages(library(SummarizedExperiment))
 se = tximeta(files)                   
 se <- addExons(se)
 gse <- summarizeToGene(se, assignRanges="abundant") #Asigna rangos según la isoforma más abundante de los transcritos en vez de desde la isoforma más en 5' a la isoforma más en 3'. Los creadores lo recomiendan.
 y <- makeDGEList(gse)
 rm(se)
-sampleinfo <- read.delim(file ="Metadata/sampleinfo_colon.txt", sep = " ", header = T)
+sampleinfo <- read.delim(file ="Metadata/sampleinfo_higado.txt", sep = "\t", header = T)
+sampleinfo[,2]
 group = sampleinfo[,2]
+#group = paste(sampleinfo$Genotipe, sampleinfo$Batch, sep = "_")
 group = factor(group)
 group = relevel(group, ref = "WT")
 y$samples$group = group
+y$samples$batch = factor(sampleinfo$Batch)
 y$samples
-design = model.matrix(~0 + group)
-colnames(design) = levels(group)
-print(design)
-keep <- filterByExpr(y, design)
+keep <- filterByExpr(y)
 print(summary(keep))
 y <- y[keep, ]
 points <- c(1:2) # Formas
 colors <- c(1:2) #Colores
-mds = plotMDS(y, col=colors[group], pch=points[group])
+mds = plotMDS(y, col=colors[group], pch=points[group], labels=y$samples$names)
 mds_data <- data.frame(
   Dim1 = mds$x,
   Dim2 = mds$y,
@@ -53,12 +54,45 @@ mds_data %>% ggplot(aes(x = Dim1, y = Dim2, col = Group, shape = Group)) +
                       colour= Group),
                   alpha = 0.2) + 
   theme(plot.title = element_text(hjust = 0.5))
+########
+logCPMs <- cpm(y, log = TRUE)
+
+# Calculate rowwise variance
+rv <- apply(logCPMs, 1, var)
+
+# Sort decreasingly and take top 1000
+o <- order(rv, decreasing=TRUE)
+top1000 <- head(o, 1000)
+
+# From the logCPMs subset for the top-1000
+logCPM_top1000 <- logCPMs[top1000,]
+
+# Run PCA
+pca <- prcomp(t(logCPM_top1000))
+
+# Combine PCA coordinates with the metadata from the DGEList
+to_plot <- data.frame(pca$x, y$samples)
+
+# Calculate how many % of total variance is explained by each principal component
+percentVar <- pca$sdev^2 / sum( pca$sdev^2 )*100
+
+# We focus here on PC1 and PC2
+use.pcs <- c(1,2)
+labs <- paste0(paste0("PC", use.pcs, " - "), paste0("Var.expl = ", round(percentVar[use.pcs], 2), "%"))
+
+ggplot(to_plot, aes(x=PC1, y=PC2, color=batch, shape=group)) + 
+  geom_point(size=3) +
+  xlab(labs[1]) + ylab(labs[2])
+###################
+design = model.matrix(~0 + group)
+colnames(design) = levels(group)
+print(design)
 y <- estimateDisp(y, design, method = "ROBUST")
 plotBCV(y)
 fit <- glmQLFit(y, design, method = "ROBUST")
 plotQLDisp(fit)
 contrast = makeContrasts(
- "KO - WT",
+  "KO - WT",
   levels=design)
 res <- glmQLFTest(fit, contrast = contrast)
 res_corrected = topTags(res, n = Inf)
@@ -72,7 +106,7 @@ ggplot(data = data, aes(x=logFC, y =-log10(PValue), col=DE, label = ifelse(abs(l
   theme_minimal() +
   geom_hline(yintercept=-log10(0.05))
 setwd("/home/guille/RNA/RNA_2024/EXPA/experimento_A/Results/")
-experimento = "exp_A_colon"
+experimento = "exp_A_higado"
 dir.create(experimento, showWarnings = FALSE)
 if (!file.exists(paste0(experimento, "/",experimento, ".xlsx"))) {
   write.xlsx2(x = res_corrected[!is.na(res_corrected$table$gene_name) & res_corrected$table$PValue <= 0.05 ,c(1,7,13,16,17)], file = paste0(experimento, "/",experimento, ".xlsx"), col.names = T, row.names = F, append = TRUE, )
@@ -81,59 +115,16 @@ if (!file.exists(paste0(experimento, "/",experimento, ".xlsx"))) {
 }
 res_corrected[1,c(1,7,13,16,17)]
 setwd("/home/guille/RNA/RNA_2024/EXPA/experimento_A")
-
 logcpm = cpm(y, log=TRUE)
 rownames(logcpm) = y$genes$symbol #Nombre de genes
-colnames(logcpm) =  paste(y$samples$group, 1:9, sep = "-")
-
-DEG = res_corrected$table[res_corrected$table$PValue < 0.05 & abs(res_corrected$table$logFC) > 1 , ]
+colnames(logcpm) =  paste(y$samples$group, y$samples$names, sep = "-")
+DEG = res_corrected$table[res_corrected$table$PValue < 0.001 & abs(res_corrected$table$logFC) > 0 , ]
 DEG_selection = logcpm[na.omit(DEG$symbol),]
 q =  as.ggplot(pheatmap(DEG_selection, scale = "row", 
                         clustering_method = "complete",
                         display_numbers = F,
-                        border_color = NA, cluster_cols = T,
-                        main = "TNAP KNOCKOUT VS WT",
+                        border_color = NA, cluster_cols = T, cutree_cols = 2, cutree_rows = 2, show_rownames = T,
                         #annotation_col = annotation, show_rownames = F, annotation_names_col = F,
                         #annotation_row = setNames(data.frame(Cluster = as.factor(cutree(q$tree_row, k=2))), "Cluster"), 
                         annotation_names_row = F,
                         legend_labels = F,))
-ggsave(filename="Results/exp_A_colon/heatmap.png", dpi = "print", bg = "white", scale = 1.80)
-
-options(enrichplot.colours = c("#FF3030CC", "#1E90FFCC"))
-foldchanges = res$table$logFC
-names(foldchanges) = res$genes$symbol
-foldchanges = sort(foldchanges, decreasing = TRUE)
-original_gene_list <- res_corrected$table$logFC
-names(original_gene_list) <- res_corrected$table$symbol
-gene_list<-na.omit(original_gene_list)
-gene_list = sort(gene_list, decreasing = TRUE)
-gse <- gseGO(geneList=na.omit(gene_list[!duplicated(names(gene_list))]), 
-             ont ="BP", 
-             keyType = "SYMBOL",
-             nPermSimple = 1000,
-             minGSSize = 3, 
-             maxGSSize = 800, 
-             pvalueCutoff = 0.05, 
-             verbose = TRUE, 
-             OrgDb = org.Mm.eg.db, 
-             pAdjustMethod = "BH")
-dotplot(gse,title = "Biological Process", split=".sign", ) + facet_grid(.~.sign)
-cnetplot(gse, categorySize="pvalue", foldChange=gene_list, showCategory = 10)
-ridgeplot(gse, showCategory = 15) + labs(x = "Enrichment Distribution", title = "Biological Process") + theme(plot.title = element_text(hjust = 0.5)) 
-
-#KEGG
-kegg_gene_list <- res_corrected$table$logFC
-names(kegg_gene_list) <- res_corrected$table$entrezid
-kegg_gene_list<-na.omit(kegg_gene_list)
-kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
-kegg_organism = "mmu"
-kk2 <- gseKEGG(geneList     = kegg_gene_list[!duplicated(names(kegg_gene_list))],
-               organism     = kegg_organism,
-               pvalueCutoff = 0.05,
-               pAdjustMethod = "BH",
-               keyType       = "ncbi-geneid")
-kk3 = setReadable(kk2, org.Mm.eg.db, keyType = "ENTREZID")
-
-dotplot(kk2,  title = "Enriched Pathways" , split=".sign") + facet_grid(.~.sign)
-cnetplot(kk3, categorySize="pvalue", foldChange=gene_list, )
-ridgeplot(kk2, showCategory = 15) + labs(x = "Enrichment Distribution", title = "KEGG Pathways") + theme(plot.title = element_text(hjust = 0.5))
